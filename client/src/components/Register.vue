fc8697b734a2a753e71b9bc27c93d13f1ecb49e8
odafe1 <godwin.otokina@ikooba.com>
2019-06-05 00:51:11 +0100

    registration started

---
+++ client/src/components/Register.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" v-model="email" name="email" placeholder="email">
+    <br>
+    <input type="password" v-model="password" name="password" placeholder="password">
+    <br> 
+    <!-- <input type="password" name="confirmPassword" placeholder="Confirm Password"> -->
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+        email: '',
+        password: ''
+    }
+  },
+  watch: {
+    //   email (value) {
+    //       console.log('email has changes', value)
+    //   }
+  },
+  methods: {
+      async register () {
+       const response = await AuthenticationService.register({
+           email: this.email,
+           password: this.password
+       })
+       console.log(response.data);   
+      }
+  },
+  mounted() {
+      
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
